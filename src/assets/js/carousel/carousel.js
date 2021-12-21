623b072fe7c5ce848f822a03f093f5c2f0186e3f
Artchibald <archie@archibaldbutler.com>
2021-12-21 13:23:03 +0100

    saved

--- src/assets/js/carousel/carousel.js
+++ src/assets/js/carousel/carousel.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './carousel.scss';
 
-// import Slide1 from './../../media/slide1.png';
-// import Slide2 from './../../media/slide2.png';
-// import Slide3 from './../../media/slide3.png';
+import Slide1 from './../../media/slide1.jpg';
+import Slide2 from './../../media/slide2.jpg';
+import Slide3 from './../../media/slide3.jpg';
 
 const Carousel = () => {
   return (
     <div className="carousel">
-      <div className="slide_1"></div>
-      <div className="slide_2"></div>
-      <div className="slide_3"></div>
+      <img src={Slide1} alt="Ryan Mitson Portfolio item" className="slide_1" />
+      <img src={Slide2} alt="Ryan Mitson Portfolio item" className="slide_2" />
+      <img src={Slide3} alt="Ryan Mitson Portfolio item" className="slide_3" />
     </div>
   );
 };
