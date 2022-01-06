bf9e41ac20f568ccf879bc4bc5e6c27180a2a343
Artchibald <archie@archibaldbutler.com>
2022-01-06 14:18:38 +0100

    img replace implemented on slide 1

--- src/assets/js/carousel/slide1.js
+++ src/assets/js/carousel/slide1.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import ProgressiveImage from './progressiveImageTemplate';
-import SlideImg1 from './../../media/slide1.jpg';
 import SlideImg1Mob from './../../media/slide1_mob.jpg';
 import Placeholder from './../../media/slide1Placeholder.jpg';
 
-const Slide1 = () => {
-  return (
-    <React.Fragment>
-      <ProgressiveImage
-        className="desk"
-        src={SlideImg1}
-        placeholder={Placeholder}
-      />
-      <ProgressiveImage
-        className="mob"
-        src={SlideImg1Mob}
-        placeholder={Placeholder}
-      />
-    </React.Fragment>
-  );
-};
+class Slide1 extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <ProgressiveImage
+          className="desk"
+          src={`https://archibaldbutler.com/projects/ryan-mitson-drag-img/slide1.jpg?${new Date().getTime()}`}
+          placeholder={Placeholder}
+        />
+        <ProgressiveImage
+          className="mob"
+          src={SlideImg1Mob}
+          placeholder={Placeholder}
+        />
+      </React.Fragment>
+    );
+  }
+}
+
 export default Slide1;
